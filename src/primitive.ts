6caf0da29ec506a8aabd2ee97bd61b8097cfda91
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-08-18 02:25:28 +0600

    Primitive data type learn done for Typescript

---
+++ src/primitive.ts
@@ -0,0 +1,31 @@
+// JS - TS
+
+// JS - Primitive: => Number, String, Boolean, Null, Undefined
+
+// TS - Primitive: never, unknown, void
+
+// String:
+let userName: string = 'pappu';
+userName = 89
+
+// Number
+let roomNumber = 45; // auto understand data type -> enplasive
+roomNumber = 90
+roomNumber = "ten"
+
+// boolean
+let isStudent: boolean = true // Menualy declear data type -> acplasively
+isStudent = false
+ 
+// any
+let x:any = undefined
+x = 90
+x = 'Ok'
+let z = undefined
+z = 89
+z = 'No'
+
+// unKnown
+
+
+ 
