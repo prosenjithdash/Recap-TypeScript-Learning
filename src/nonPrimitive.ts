3329fc20341e7d4e278d7bbde9868925cd2e1a1a
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-08-25 17:58:40 +0600

    Object, Literal & Optional Type done

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -24,3 +24,36 @@ let coordinatesE: [number, number] = [60, 70, 80] // not allow need just 2 num
 let bud: [number, string, number] = [90, 'bud', 100]
 let Dog:[number,string,number] = [90,100,'dog'] // must maintain serial
 
+
+// reference type : object
+
+const user: {
+    firstName: string;
+    middleName?: string; // optional type
+    lastName: string;
+} = {
+    firstName: 'Prosenjith',
+    middleName: 'Dash',
+    lastName: 'Pappu'
+    
+}
+
+// Value => type
+
+const info: {
+// TYPE DEFINE
+    company: 'PDP Ltd', //value => type : Literal types
+    city: string,
+    division: string
+} = {
+    company: 'PDP Ltd',
+    city: 'Dhanmondi',
+    division : 'Dhaka'
+}
+
+info.company = 'PDB Ltd' // don't change this value course of value is define as a type
+
+//Another option is readonly. When you will use readonly then you can not change any value.
+//like that
+
+  // readonly company: string,
