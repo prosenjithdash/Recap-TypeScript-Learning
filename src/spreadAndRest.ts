2bf1b6e9df229e66307574fc8b33525a4da91582
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-08-27 10:48:26 +0600

    rest operator done

--- src/spreadAndRest.ts
+++ src/spreadAndRest.ts
@@ -27,6 +27,29 @@ console.log(userInfo)
 
 
 // rest operator
+const friendsInvite = (frn1: string, frn2: string, frn3: string) => {
+    // here invitation send single single
+    console.log(`Sent Invitation to ${frn1}`)
+    console.log(`Sent Invitation to ${frn2}`)
+    console.log(`Sent Invitation to ${frn3}`)
+}
+
+friendsInvite('Asig', 'Alok', 'Abir');
+
+// why use rest operator?
+// when use loop then need to array
+// if you want make array then must use rest operator like => ...tourP
+// like that => ['Himel', 'Siam', 'Ashok','Avi']
+
+const tourPlan = (...tourP:string[]) => {
+    // here invitation send with forEach loop
+    tourP.forEach((tourP:String) => console.log(`Send Tour plan to ${tourP}`))
+
+}
+
+tourPlan('Himel', 'Siam', 'Ashok','Avi');
+
+
 
 
 
