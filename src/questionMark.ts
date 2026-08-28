9fec25535d9de028d62ee580dfe1def9b1e4844f
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-08-29 02:46:19 +0600

    ?. : optional chaining done

--- src/questionMark.ts
+++ src/questionMark.ts
@@ -1,8 +1,10 @@
 // ? : ternary operator : decision making
-// ?? : nullish coaLessing operator
+// ?? : nullish coaLessing operator : null / undefined
 // ?. : optional chaining
 // code run type on terminal=> npx tsx ./src/questionMark.ts
 
+
+// ? : ternary operator : decision making
 // using if , else
 const MerageEligible = (age : number) => {
     if (age >= 21) {
@@ -15,10 +17,62 @@ const MerageEligible = (age : number) => {
 MerageEligible(21);
 
 // using ternary operator (? :)
-const JobEligible = (interviewPoint:number) => {
+const JobEligible = (interviewPoint: number) => {
+    
     const result = interviewPoint >= 80 ? 'You are eligible for this job.' : 'You are not eligible for this job.'
     console.log(result)
     
 }
 
 JobEligible(75);
+
+
+// ?? : nullish coaLessing operator : null / undefined
+// => for null or undefined value result is default value
+//=> without null , undefined  or blank value then result is emty.
+//=> if have to any value then showing that like -> const userTheme =  'Green Theme'
+
+// for undefined
+const userTheme = undefined;
+const selectedTheme = userTheme ?? 'Light Theme.'
+console.log(selectedTheme)
+
+// for null
+const guestTheme = null;
+const setTheme = guestTheme ?? 'Dark Theme.'
+console.log(setTheme)
+
+
+// for blank value without null , undefined , value
+const employeeTheme = '';
+const employeeSetTheme = employeeTheme ?? 'Dark Theme.'
+console.log(employeeSetTheme)
+
+// for value without null or undefined
+const adminTheme = 'Green Theme';
+const adminSetTheme = adminTheme ?? 'Dark Theme.'
+console.log(adminSetTheme)
+
+
+//Optional Chaining ?
+const Employee: {
+    id: number,
+    name: string,
+    address: {
+        city: string,
+        division: string,
+        area: string
+    }
+} = {
+    id: 898,
+    name: 'Ashuk Dash',
+    address: {
+        city: 'Dhaka',
+        division: 'Dhaka'
+    }
+}
+const area = Employee?.address?.area
+console.log(area);
+
+
+
